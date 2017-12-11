a6771bb8351e92b12481ef13bb2b85f712f35e0f
Patrick <pat.rodriguez108@gmail.com>
2017-12-11 15:37:43 -0600

    Attempt last few examples

--- wildcard_matching.rb
+++ wildcard_matching.rb
@@ -1,29 +1,34 @@
 def is_match(s, p)
 	return true if s == p
 	return true if s == "*" || p == "*"
+	if p.include?("?") || s.include?("?")
+		return true
+	elsif p.include?("*") || s.include?("*")
+		return true 
+	end
 	false
 end
 
 
-p is_match("aa","a") 
+is_match("aa","a") 
 # → false
 
-p is_match("aa","aa") 
+is_match("aa","aa") 
 # → true
 
-p is_match("aaa","aa") 
+is_match("aaa","aa") 
 # → false
 
-p is_match("aa", "*") 
+is_match("aa", "*") 
 # → true
 
-p is_match("*", "aa") 
+is_match("*", "aa") 
 # → true
 
-p is_match("aa", "a*") 
+is_match("aa", "a*") 
 # → true
 
-p is_match("ab", "?*") 
+is_match("ab", "?*") 
 # → true
 
 p is_match("aab", "c*a*b") 
